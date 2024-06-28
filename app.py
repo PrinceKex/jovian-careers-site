15108dcb6c37b6813c7dca33a0dc9ec56906d0f3
PrinceKex <ehigiator.ukeke@gmail.com>
2024-06-28 09:39:45 +0100

    added job route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -28,9 +28,13 @@ JOBS = [
   }
 ]
 
-@app.route('/')
-def hello_world():
-  return render_template('home.html')
+@app.route("/")
+def hello_jovian():
+    return render_template('home.html',                         jobs=JOBS, company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
